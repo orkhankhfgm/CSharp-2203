2321add773aaf19ae6b9d9ba0894aa831d22dad1
Orkhan Farajov <orkhankhf@gmail.com>
2022-04-29 00:34:00 +0400

    OOP Classes

---
+++ Aprel/29/OOP - Classes/OOP - Classes/Customer.cs
@@ -0,0 +1,67 @@
+﻿namespace OOP___Classes
+{
+    class Customer
+    {
+        public string Name;
+        public string Surname;
+        public int Gender; //1: Kisi 2: Qadin
+        public string DocumentSerial { get; set; }
+        public string DocumentNumber { get; set; }
+        public string Address { get; set; }
+
+        public Customer()
+        {
+            DocumentNumber = "000000";
+            DocumentSerial = "AZ";
+        }
+
+        public Customer(string documentNumber)
+        {
+            DocumentNumber = documentNumber;
+        }
+
+        public Customer(string documentNumber, string documentSerial)
+        {
+            DocumentNumber = documentNumber;
+            DocumentSerial = documentSerial;
+        }
+
+        public Customer(string documentNumber, string documentSerial, string name, string surname)
+        {
+            DocumentNumber = documentNumber;
+            DocumentSerial = documentSerial;
+            Name = name;
+            Surname = surname;
+        }
+
+        public Customer(int gender, string name, string surname)
+        {
+            Gender = gender;
+            Name = name;
+            Surname = surname;
+        }
+
+        public Customer(string documentNumber, string documentSerial, int gender, string name, string surname, string address)
+        {
+            DocumentNumber = documentNumber;
+            DocumentSerial = documentSerial;
+            Gender = gender;
+            Name = name;
+            Surname = surname;
+            Address = address;
+        }
+
+
+
+        private bool CheckCustomerFromDb()
+        {
+            return true;
+        }
+
+        public bool CheckCustomer()
+        {
+            bool result = CheckCustomerFromDb();
+            return result;
+        }
+    }
+}
---
+++ Aprel/29/OOP - Classes/OOP - Classes/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP___Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Müştəri
+            //Satış Meneceri
+            //Satılacaq məhsul (Avtomobil, moto, gəmi)
+            //Təklif
+            //Sifariş
+            //Qəbz
+
+
+            /*
+            Müştəri
+                Ad
+                Soyad
+                Cinsiyyət
+                Pasport seriya və nömrə
+                Ünvanı
+            
+            Məhsul
+                Marka
+                Model
+                İl
+                Km
+                Yanacaq növü
+                Ban növü
+            
+            Təklif
+                Təklif nömrəsi
+                Müştəri
+                Satıcı
+                Məhsul
+                Qiymət
+            
+            Sifariş
+                Sifariş nömrəsi
+                Təklif
+                Qiymət
+
+            Qəbz
+                Vergi nömrəsi
+                Qəbz nömrəsi
+                Seriya
+                Təklif
+                Müştəri
+             */
+
+            string name = "Orkhan";
+
+            Customer customer = new Customer();
+            customer.Name = "Orkhan";
+            customer.Surname = "Farajov";
+            customer.Gender = 1;
+            customer.Address = "Baki, Nerimanov, Rasim Axundov, No 5";
+
+
+            Customer otherCustomer = new Customer();
+            otherCustomer = customer;
+
+            otherCustomer.Name = "Name changed for otherCustomer";
+            customer.Surname = "Surname changed for customer";
+
+            customer = null;
+            otherCustomer = null;
+
+            //string customerName = otherCustomer.Name;
+
+
+            Customer defaultCtor = new Customer();
+            Customer overload1 = new Customer("123456");
+            Customer overload2 = new Customer("123456", "AZ");
+            Customer overload3 = new Customer("123456", "AZ", "Orkhan", "Farajov");
+            Customer overload4 = new Customer(1, "Orkhan", "Farajov");
+            Customer overload5 = new Customer("123456", "AZ", 1, "Orkhan", "Farajov", "Baki seh.");
+
+            overload1.CheckCustomer();
+
+
+        }
+    }
+}
