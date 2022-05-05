382dc4689511cc2964c7510473452cfdbf9992fb
Orkhan Farajov <orkhankhf@gmail.com>
2022-05-06 00:36:29 +0400

    Encapsulation

---
+++ May/06/Encapsulation/Encapsulation/Customer.cs
@@ -0,0 +1,23 @@
+﻿namespace Encapsulation
+{
+    class Customer
+    {
+        public int Id;
+        public string Name;
+        public string Surname;
+        private string email;
+
+        public string Email {
+            get
+            {
+                return this.email;
+            }
+            set
+            {
+                this.email = value;
+            }
+        }
+
+        public string ShortGetSet { get; set; }
+    }
+}
---
+++ May/06/Encapsulation/Encapsulation/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+            customer.Id = 1;
+            customer.Name = "Orkhan";
+            customer.Surname = "Farajov";
+            customer.Email = "orkhan@gmail.com";
+            var email = customer.Email;
+
+            Student student = new Student();
+            student.DocumentSerial = "AZ";
+            student.DocumentNumber = "12345678";
+
+            var a = student.DocumentSerial;
+            var b = student.DocumentNumber;
+        }
+    }
+}
